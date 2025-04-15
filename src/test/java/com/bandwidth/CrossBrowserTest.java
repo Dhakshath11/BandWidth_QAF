8d3d80dbc78222f2d9ba685ca74b78e9f5914fb5
Dhakshath11 <110535591+DhakshathAmin@users.noreply.github.com>
2025-04-15 18:35:55 +0530

    Initial commit

---
+++ src/test/java/com/bandwidth/CrossBrowserTest.java
@@ -0,0 +1,38 @@
+package com.bandwidth;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.SkipException;
+import org.testng.annotations.*;
+
+import com.bandwidth.commons.FrameworkGlobalVariables;
+import com.bandwidth.commons.Driver.WebDriverFactory;
+
+public class CrossBrowserTest {
+
+	WebDriver driver;
+
+	@Parameters({ "browser", "hubURL" })
+	@BeforeMethod
+	public void setup(String browser, String hubURL) throws Exception {
+		FrameworkGlobalVariables.EXECUTION_ENV = "LOCAL";
+		driver = WebDriverFactory.createInstance(browser, hubURL);
+
+		if (driver == null)
+			throw new SkipException("Driver is NULL. Execution skipped");
+	}
+
+	@Test
+	public void launchSiteAndClick() throws InterruptedException {
+		driver.get("https://example.com");
+		Thread.sleep(3000);
+		driver.findElement(By.tagName("a")).click(); // Update selector as needed
+		Thread.sleep(5000);
+	}
+
+	@AfterMethod
+	public void tearDown() {
+		if (driver != null)
+			driver.quit();
+	}
+}
---
+++ src/test/java/com/bandwidth/commons/FrameworkGlobalVariables.java
@@ -0,0 +1,5 @@
+package com.bandwidth.commons;
+
+public class FrameworkGlobalVariables {
+	public static String EXECUTION_ENV = "LOCAL";
+}
