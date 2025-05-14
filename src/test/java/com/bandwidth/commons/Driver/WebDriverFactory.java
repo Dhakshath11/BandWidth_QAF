1f128e9e71d60680fe1456c10124ad11ada67795
Dhakshath11 <dakshathamin11@gmail.com>
2025-05-14 13:35:35 +0530

    Removal of LT_USERNAME & LT_ACCESS_KEY from Code as it was hard-coded. Reading from Env variables

--- src/test/java/com/bandwidth/commons/Driver/WebDriverFactory.java
+++ src/test/java/com/bandwidth/commons/Driver/WebDriverFactory.java
@@ -14,27 +14,31 @@ import com.bandwidth.commons.Report.TestListener;
 
 public class WebDriverFactory {
 
-	public static WebDriver createInstance() throws Exception {
-		try {
-			String env = TestCaseInputs.getEnv();
-			TestListener.getTest().log(Status.INFO, "Creating Driver Instance for " + env + "environment");
-			
-			if (env.equalsIgnoreCase("LOCAL")) {
-				String hubURL = FrameworkGlobalVariables.HUB_URL;
-				return new RemoteWebDriver(new URL(hubURL), new LocalExecutionCapabilities().getDesiredCapabilities());
+    public static WebDriver createInstance() throws Exception {
+        try {
+            String env = TestCaseInputs.getEnv();
+            TestListener.getTest().log(Status.INFO, "Creating Driver Instance for " + env + "environment");
 
-			} else if (env.equalsIgnoreCase("LAMBDATEST")) {
-				String gridURL = "https://" + FrameworkGlobalVariables.LT_USERNAME + ":"
-						+ FrameworkGlobalVariables.LT_ACCESS_KEY + FrameworkGlobalVariables.LT_GRID_URL;
-				return new RemoteWebDriver(new URL(gridURL), new CloudExecutionCapabilities().getDesiredCapabilities());
-			} else {
-				System.out.println(env + " is not a supported type of environment.");
-				return null;
-			}
-		} catch (Exception e) {
-			System.out.println(e.getLocalizedMessage());
-			return null;
-		}
-	}
+            if (env.equalsIgnoreCase("LOCAL")) {
+                String hubURL = FrameworkGlobalVariables.HUB_URL;
+                return new RemoteWebDriver(new URL(hubURL), new LocalExecutionCapabilities().getDesiredCapabilities());
+
+            } else if (env.equalsIgnoreCase("LAMBDATEST")) {
+                if (FrameworkGlobalVariables.LT_USERNAME.isEmpty() || FrameworkGlobalVariables.LT_ACCESS_KEY.isEmpty()) {
+                    System.out.println("Please set LT_USERNAME & LT_ACCESS_KEY inside FrameworkGlobalVariables. It is recommend to set it from SYSTEM-ENV Variables");
+                    return null;
+                }
+                String gridURL = "https://" + FrameworkGlobalVariables.LT_USERNAME + ":"
+                        + FrameworkGlobalVariables.LT_ACCESS_KEY + FrameworkGlobalVariables.LT_GRID_URL;
+                return new RemoteWebDriver(new URL(gridURL), new CloudExecutionCapabilities().getDesiredCapabilities());
+            } else {
+                System.out.println(env + " is not a supported type of environment.");
+                return null;
+            }
+        } catch (Exception e) {
+            System.out.println(e.getLocalizedMessage());
+            return null;
+        }
+    }
 
 }
--- src/test/java/com/bandwidth/commons/Inputs/FrameworkGlobalVariables.java
+++ src/test/java/com/bandwidth/commons/Inputs/FrameworkGlobalVariables.java
@@ -1,9 +1,26 @@
 package com.bandwidth.commons.Inputs;
 
 public class FrameworkGlobalVariables {
-	public static String LT_USERNAME = "dhakshathamin";
-	public static String LT_ACCESS_KEY = "LT_Pf3gLJhEz0t7X2h7gQca0y2fAzQJVUl6SwRxPNPlsPatcXf";
+	public static String LT_USERNAME = "";
+	public static String LT_ACCESS_KEY = "";
 	public static String LT_GRID_URL = "@hub.lambdatest.com/wd/hub";
 	public static String HUB_URL = "http://localhost:4444/wd/hub";
 
+	public static void loadLTEnvVariables() {
+		// First check System environment variables
+		String username = System.getenv("LT_USERNAME");
+		String accessKey = System.getenv("LT_ACCESS_KEY");
+
+		// If not found in environment, check JVM system properties
+		if (username == null || username.isEmpty()) {
+			username = System.getProperty("LT_USERNAME", "");
+		}
+
+		if (accessKey == null || accessKey.isEmpty()) {
+			accessKey = System.getProperty("LT_ACCESS_KEY", "");
+		}
+
+		LT_USERNAME = username;
+		LT_ACCESS_KEY = accessKey;
+	}
 }
